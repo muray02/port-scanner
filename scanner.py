6e7c7a96d6877f76df389dee32f5ae9e7c59719e
muray02 <muratlageza540@gmail.com>
2025-06-15 18:12:53 +0600

    Добавлен ui интерфейс

--- scanner.py
+++ scanner.py
@@ -1,11 +1,15 @@
-import socket
+ï»¿import socket
 
 def scan(host, ports):
+    open_ports = []
     for port in ports:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(0.5)
-        result = sock.connect.ex((host, port))
-        if result == 0:
-            print(f"[+] Ïîðò {port} îòêðûò")
-        sock.close()
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                sock.settimeout(0.5)
+                result = sock.connect_ex((host, port))
+                if result == 0:
+                    open_ports.append(port)
+        except:
+            pass
+    return open_ports
 
---
+++ ui.py
@@ -0,0 +1,41 @@
+﻿from tkinter import *
+from scanner import scan
+
+def scan_ports():
+    ip = ip_entry.get()
+    port_start = int(port_start_entry.get())
+    port_end = int(port_end_entry.get())
+    ports = range(port_start, port_end + 1)
+
+    result = scan(ip, ports)  
+
+    if result:
+        output_label.config(text=f"Открытые порты: {', '.join(map(str, result))}")
+    else:
+        output_label.config(text="Открытых портов не найдено")
+
+root = Tk()
+
+root.title("PortScanner")
+root.geometry("300x400")
+
+# Press button
+button = Button(root, text="Сканировать порты", font=30, command=scan_ports)
+button.pack(side=BOTTOM, pady=40)
+
+Label(root, text="IP-adress").pack()
+ip_entry = Entry(root)
+ip_entry.pack()
+
+Label(root, text="Начальный порт").pack()
+port_start_entry = Entry(root)
+port_start_entry.pack()
+
+Label(root, text="Конечный порт").pack()
+port_end_entry = Entry(root)
+port_end_entry.pack()
+
+output_label = Label(root, text="")
+output_label.pack()
+
+root.mainloop()
